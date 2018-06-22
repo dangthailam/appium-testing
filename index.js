66ef3a62d89421ad170b3ee93e8c2e7676ec7d5b
Thai-lam DANG <lamtp1989@gmail.com>
2018-06-22 11:39:10 +0200

    add logging; add chai setup; and FIRST test passed

--- index.js
+++ index.js
@@ -1,36 +1,65 @@
+require('./setup');
+
 const wd = require("wd");
-let driver;
+
 const opts = {
     port: 4723
 };
 const desired = {
-    platformName: "Android",
-    platformVersion: "8.0",
-    deviceName: "Android Emulator",
-    app: "D:/Lam/projects/appium-test/app-prod-debug.apk",
-    automationName: "UiAutomator2"
+    "platformName": "Android",
+    "platformVersion": "8.0",
+    "deviceName": "Android Emulator",
+    "app": "D:/Lam/projects/appium-test/app-prod-debug.apk",
+    "automationName": "UiAutomator2",
+    "appWaitActivity": "*.LoginActivity"
 };
 
 
 describe("Identification user", function() {
+    this.timeout(300000);
+    let driver;
+
     before(function() {
         driver = wd.promiseChainRemote(opts);
+        require("./logging").configure(driver);
         return driver
             .init(desired)
-            .setImplicitWaitTimeout(3000);
+            .setImplicitWaitTimeout(30000);
     });
 
-    it("Login with email and password", function() {
-        console.log('login with email and password');
-        driver
-            .elementsByAndroidUIAutomator('new UiSelector().resourceId("fr.vestiairecollective:id/btn_connect_vestiaire")')
+    after(function() {
+        return driver
+            .quit()
+            .finally(function() {
+                console.log('finish');
+            });
+    });
+
+    it("Login with email and password", function(done) {
+        driver.elementById("fr.vestiairecollective:id/btn_connect_vestiaire")
             .should.eventually.exist
             .click()
-            .sleep(1000)
-            .elementsByAndroidUIAutomator('new UiSelector().resourceId("fr.vestiairecollective:id/edit_login_username")')
+            .elementById("fr.vestiairecollective:id/edit_login_username")
             .should.eventually.exist
-            .clear()
             .sendKeys("lebichngoc090589@gmail.com")
-            .end();
+            .elementById("fr.vestiairecollective:id/edit_login_password")
+            .should.eventually.exist
+            .sendKeys("09051989")
+            .elementById("fr.vestiairecollective:id/btn_connect_from_vestiaire")
+            .should.eventually.exist
+            .click()
+            .sleep(4000)
+            .nodeify(done);
+        // let el2 = await driver.elementById("fr.vestiairecollective:id/edit_login_username");
+        // await el2.sendKeys("lebichngoc090589@gmail.com");
+
+        // driver
+        //     .elementsByAndroidUIAutomator('new UiSelector().resourceId("fr.vestiairecollective:id/btn_connect_vestiaire")')
+        //     .click()
+        //     .sleep(5000)
+        //     .elementsByAndroidUIAutomator('new UiSelector().resourceId("fr.vestiairecollective:id/edit_login_username")')
+        //     .clear()
+        //     .sendKeys("lebichngoc090589@gmail.com")
+        //     .sleep(10000);
     });
 });
