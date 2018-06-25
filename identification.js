108f0c5c21c58ffe7973f314b9e29a5cc5c89ae3
Ngoc Thi Bich Le <ngoc.le@vdc.lan>
2018-06-25 16:14:58 +0200

    Ngoc-Identification

--- identification.js
+++ identification.js
@@ -9,25 +9,27 @@ const desired = {
     "platformName": "Android",
     "platformVersion": "8.0",
     "deviceName": "Android Emulator",
-    "app": "D:/Lam/projects/appium-test/app-prod-debug.apk",
+    "app": "C:/appium/appium-testing/app-prod-debug.apk",
     "automationName": "UiAutomator2",
     "appWaitActivity": "*.LoginActivity"
 };
 
-
 describe("Identification user", function() {
     this.timeout(300000);
     let driver;
+    let passed = false;
 
     before(function() {
         driver = wd.promiseChainRemote(opts);
         require("./logging").configure(driver);
         return driver
-            .init(desired)
-            .setImplicitWaitTimeout(30000);
+            .init(desired);
     });
 
     after(function() {
+        if(!passed){
+            console.log("not passed");
+        }
         return driver
             .quit()
             .finally(function() {
@@ -39,6 +41,7 @@ describe("Identification user", function() {
         driver.elementById("fr.vestiairecollective:id/btn_connect_vestiaire")
             .should.eventually.exist
             .click()
+            .sleep(1000)
             .elementById("fr.vestiairecollective:id/edit_login_username")
             .should.eventually.exist
             .sendKeys("lebichngoc090589@gmail.com")
@@ -49,17 +52,17 @@ describe("Identification user", function() {
             .should.eventually.exist
             .click()
             .sleep(4000)
+            .elementById("fr.vestiairecollective:id/img_home")
+            .should.eventually.exist
+            .elementById("fr.vestiairecollective:id/badge_icon_button")
+            .should.eventually.exist
+            .elementById("fr.vestiairecollective:id/animated_search")    
+            .should.eventually.exist
+            .elementById("fr.vestiairecollective:id/homeList") 
+            .should.eventually.exist
+            .elementById("fr.vestiairecollective:id/navigation") 
+            .should.eventually.exist
             .nodeify(done);
-        // let el2 = await driver.elementById("fr.vestiairecollective:id/edit_login_username");
-        // await el2.sendKeys("lebichngoc090589@gmail.com");
-
-        // driver
-        //     .elementsByAndroidUIAutomator('new UiSelector().resourceId("fr.vestiairecollective:id/btn_connect_vestiaire")')
-        //     .click()
-        //     .sleep(5000)
-        //     .elementsByAndroidUIAutomator('new UiSelector().resourceId("fr.vestiairecollective:id/edit_login_username")')
-        //     .clear()
-        //     .sendKeys("lebichngoc090589@gmail.com")
-        //     .sleep(10000);
+            
     });
 });
