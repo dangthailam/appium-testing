b54459685a57796f2ce8309dc1b35f8b9aeaa030
Ngoc Thi Bich Le <ngoc.le@imac-029.vdc.lan>
2018-07-23 15:31:05 +0200

    commentaire 2/3

--- ios/06-editorialContent-commentaire-ios.js
+++ ios/06-editorialContent-commentaire-ios.js
@@ -36,6 +36,76 @@ describe("Contenus édito / Commentaire", function () {
         allPassed = allPassed && this.currentTest.state === 'passed';
         return driver.quit();
     });
+    it("Vérifier que l'on peut cliquer sur les contenus édito", function (done) {
+        _shared.methods.shouldLogin(driver)
+        // driver.sleep(500)
+            .then(function () {
+                return _shared.methods.swipeBottomUpAndCheckIfElementExist(driver, "//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[3]/XCUIElementTypeOther/XCUIElementTypeImage[1]")
+            })
+            .should.eventually.exist
+            .click()
+            // Ne pouvoir pas vérifier le contenu car ça change tout le temps > element change
+            .elementByAccessibilityId("Back")
+            .should.eventually.exist
+            .click()
+            .sleep(500)
+            .then(function () {
+                return _shared.methods.swipeBottomUpAndCheckIfElementExist(driver, "//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[3]/XCUIElementTypeOther/XCUIElementTypeImage[1]")
+            })
+            .should.eventually.exist
+            .click()
+            .elementByAccessibilityId("Back")
+            .should.eventually.exist
+            .click()
+            .sleep(500)
+            .then(function () {
+                return _shared.methods.swipeBottomUpAndCheckIfElementExist(driver,"//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[3]/XCUIElementTypeOther/XCUIElementTypeImage[1]")
+            })
+            .should.eventually.exist
+            .click()
+            .elementByAccessibilityId("Back")
+            .should.eventually.exist
+            .click()
+            .sleep(500)
+            .then(function () {
+                return _shared.methods.swipeBottomUpAndCheckIfElementExist(driver,"//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[3]/XCUIElementTypeOther/XCUIElementTypeImage[1]")
+            })
+            .should.eventually.exist
+            .click()
+            .elementByAccessibilityId("Back")
+            .should.eventually.exist
+            .click()
+            .sleep(500)
+            .then(function () {
+                return _shared.methods.swipeBottomUpAndCheckIfElementExist(driver,"//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeOther[3]/XCUIElementTypeButton")
+            })
+            .should.eventually.exist
+            .click()
+            .waitForElementByAccessibilityId("Back",500)
+            .should.eventually.exist
+            .elementByXPath("//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeOther[3]/XCUIElementTypeButton")
+            .should.eventually.exist
+            .nodeify(done);
+
+            // .elementByAccessibilityId("WRITE A COMMENT")
+            // .should.eventually.exist
+            // .click()
+            // .waitForElementByXPath("//XCUIElementTypeOther[@name=\"Chat\"]", 500)
+            // .should.eventually.exist
+            // .elementByAccessibilityId("Add a comment...")
+            // .sendKeys("test")
+            // .elementByAccessibilityId("Send")
+            // .click()
+            // .waitForElementByXPath("//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[2]", 500)
+            // .should.eventually.exist
+            // .elementByAccessibilityId("Close")
+            // .should.eventually.exist
+            // .click()
+            // .waitForElementByAccessibilityId("WRITE A COMMENT", 500)
+            // .should.eventually.exist
+            // .nodeify(done);
+
+    });
 
     it("Vérifier que l'on peut écrire les commentaires", function (done) {
         //_shared.methods.shouldLogin(driver)
@@ -65,125 +135,5 @@ describe("Contenus édito / Commentaire", function () {
             .nodeify(done);
     });
 
-    it.skip("Vérifier que l'on peut cliquer sur les contenus édito", function (done) {
-        _shared.login.shouldLogin(driver)
-            .elementByAccessibilityId("Home")
-            .should.eventually.exist
-            .click()
-            .sleep(500)
-            .swipe({
-                startX: 400,
-                startY: 600,
-                endX: 200,
-                endY: 200,
-                duration: 800
-            })  // KO swipe
-            .elementByXPath("//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[3]/XCUIElementTypeOther/XCUIElementTypeImage[1]")
-            .should.eventually.exist
-            .click()
-            .elementByXPath("//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView")
-            .should.eventually.exist
-            .elementByAccessibilityId("btn-cart")
-            .should.eventually.exist
-            .elementByAccessibilityId("Back")
-            .should.eventually.exist
-            .click()
-            .sleep(500)
-            .swipe({
-                startX: 400,
-                startY: 600,
-                endX: 200,
-                endY: 200,
-                duration: 1000
-            })
-            .elementByXPath("//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[3]/XCUIElementTypeOther/XCUIElementTypeImage[1]")
-            .should.eventually.exist
-            .click()
-            .waitForElementByXPath("//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView", 500)
-            .should.eventually.exist
-            .elementByAccessibilityId("btn-cart")
-            .should.eventually.exist
-            .elementByAccessibilityId("Back")
-            .should.eventually.exist
-            .click()
-            .sleep(500)
-            .swipe({
-                startX: 400,
-                startY: 600,
-                endX: 200,
-                endY: 200,
-                duration: 1000
-            })
-            .elementByXPath("//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[3]/XCUIElementTypeOther/XCUIElementTypeImage[1]")
-            .should.eventually.exist
-            .click()
-            .waitForElementByAccessibilityId("main", 500)
-            .should.eventually.exist
-            .elementByAccessibilityId("Back")
-            .should.eventually.exist
-            .click()
-            .sleep(500)
-            .swipe({
-                startX: 400,
-                startY: 600,
-                endX: 200,
-                endY: 200,
-                duration: 1000
-            })
-            .elementByXPath("//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[3]/XCUIElementTypeOther/XCUIElementTypeImage[1]")
-            .should.eventually.exist
-            .click()
-            .waitForElementByXPath("//XCUIElementTypeNavigationBar[@name=\"Sell\"]", 500)
-            .should.eventually.exist
-            .elementByAccessibilityId("btn-sell")
-            .should.eventually.exist
-            .elementByAccessibilityId("Home")
-            .should.eventually.exist
-            .click()
-            .sleep(500)
-            .swipe({
-                startX: 400,
-                startY: 600,
-                endX: 200,
-                endY: 200,
-                duration: 1000
-            })
-            .elementByXPath("//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeOther[3]/XCUIElementTypeButton")
-            .should.eventually.exist
-            .click()
-            .waitForElementByAccessibilityId("main", 500)
-            .should.eventually.exist
-            .elementByAccessibilityId("Back")
-            .should.eventually.exist
-            .click()
-            .waitForElementByXPath("//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeTextField", 500)
-            .should.eventually.exist
-            .sendKeys("5731296")
-            .sleep(500)
-            .swipe({
-                startX: 400,
-                startY: 600,
-                endX: 200,
-                endY: 200,
-                duration: 1000
-            })
-            .elementByAccessibilityId("WRITE A COMMENT")
-            .should.eventually.exist
-            .click()
-            .waitForElementByXPath("//XCUIElementTypeOther[@name=\"Chat\"]", 500)
-            .should.eventually.exist
-            .elementByAccessibilityId("Add a comment...")
-            .sendKeys("test")
-            .elementByAccessibilityId("Send")
-            .click()
-            .waitForElementByXPath("//XCUIElementTypeApplication[@name=\"Vestiaire\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[2]", 500)
-            .should.eventually.exist
-            .elementByAccessibilityId("Close")
-            .should.eventually.exist
-            .click()
-            .waitForElementByAccessibilityId("WRITE A COMMENT", 500)
-            .should.eventually.exist
-            .nodeify(done);
-
-    });
+    
 });
